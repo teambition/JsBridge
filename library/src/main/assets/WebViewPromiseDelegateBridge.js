72e736c05368f3c2bba40b6c96818da17a78ffa5
qianqian <zhouqian@teambition.com>
2019-03-06 11:47:33 +0800

    code review

--- library/src/main/assets/WebViewPromiseDelegateBridge.js
+++ library/src/main/assets/WebViewPromiseDelegateBridge.js
@@ -5,45 +5,45 @@
   }
 
   function connectWebViewJavascriptBridge(callback) {
-      if (window.WebViewJavascriptBridge) {
-        callback(WebViewJavascriptBridge);
-      } else {
-        document.addEventListener(
-          'WebViewJavascriptBridgeReady',
-          function() {
-            callback(WebViewJavascriptBridge);
-          }, false);
-      }
+    if (window.WebViewJavascriptBridge) {
+      callback(WebViewJavascriptBridge);
+    } else {
+      document.addEventListener(
+        'WebViewJavascriptBridgeReady',
+        function() {
+          callback(WebViewJavascriptBridge);
+        }, false);
     }
+  }
 
-    connectWebViewJavascriptBridge(function(bridge) {
-        var TeambitionMobileSDK = window.TeambitionMobileSDK = {
-        call: function(funcName, jsonRequest) {
-          return new Promise(function(resolve, reject) {
-            const requestStr = JSON.stringify(jsonRequest);
-            bridge.callHandler(funcName, requestStr,
-                      function(bridgeResponse) {
-                        const response = JSON.parse(bridgeResponse);
-                        if (response.isSuccess) {
-                          resolve(response.data);
-                        } else {
-                          let err = {
-                            name: response.error.name,
-                            message: response.error.message
-                          };
-                          reject(err);
-                        }
-                      }
-                    );
-          });
-        }
-      };
+  connectWebViewJavascriptBridge(function(bridge) {
+    var TeambitionMobileSDK = window.TeambitionMobileSDK = {
+      call: function(funcName, jsonRequest) {
+        return new Promise(function(resolve, reject) {
+          const requestStr = JSON.stringify(jsonRequest);
+          bridge.callHandler(funcName, requestStr,
+            function(bridgeResponse) {
+              const response = JSON.parse(bridgeResponse);
+              if (response.isSuccess) {
+                resolve(response.data);
+              } else {
+                let err = {
+                  name: response.error.name,
+                  message: response.error.message
+                };
+                reject(err);
+              }
+            }
+          );
+        });
+      }
+    };
 
-      var readyEvent = document.createEvent('Events');
-      readyEvent.initEvent('TeambitionMobileSDKReady');
-      readyEvent.TeambitionMobileSDK = TeambitionMobileSDK;
-      document.dispatchEvent(readyEvent);
+    var readyEvent = document.createEvent('Events');
+    readyEvent.initEvent('TeambitionMobileSDKReady');
+    readyEvent.TeambitionMobileSDK = TeambitionMobileSDK;
+    document.dispatchEvent(readyEvent);
 
-    });
+  });
 
 })();
